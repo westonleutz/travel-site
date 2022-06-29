a345ede9a4e992f1d94365191c10199e30c39639
Weston <wes.leutz@gmail.com>
2022-06-29 18:27:14 -0500

    Section 6 : Lesson 20

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
+  devServer: {
+    watchFiles: ["./app/**/*.html"],
+    static: {
+      directory: path.resolve(__dirname, 'app'),
+    },
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
   mode: 'development',
-  watch: true,
   module: {
     rules: [
       {
