d0db48eeafe95e8893c4018f54234d1de8e1a2f1
Weston <wes.leutz@gmail.com>
2022-07-25 16:43:40 -0500

    Finished Reveal Items

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,13 @@
 import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
 
 if (module.hot) {
   module.hot.accept()
 }
 
 /* Lesson code */
-let mobileMenu = new MobileMenu();
+let mobileMenu = new MobileMenu()
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,55 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class RevealOnScroll {
+  constructor(els, thresholdPercent) {
+    this.thresholdPercent = thresholdPercent
+    this.ItemsToReveal = els
+    this.browserHeight = window.innerHeight
+    this.hideInitially()
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+    this.events()
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener("resize", debounce(() => {
+      console.log("Resize just ran")
+      this.browserHeight = window.innerHeight
+    }, 333))
+  }
+
+  calcCaller() {
+    this.ItemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTo(el)
+      }
+    })
+  }
+
+  calculateIfScrolledTo(el) {
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100
+
+      if (scrollPercent < this.thresholdPercent) {
+        el.classList.add("reveal-item--is-visible")
+        el.isRevealed = true
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle)
+        }
+      }
+    }
+
+  }
+
+  hideInitially() {
+    this.ItemsToReveal.forEach(el => {
+      el.classList.add("reveal-item")
+      el.isRevealed = false
+    })
+
+    this.ItemsToReveal[this.ItemsToReveal.length - 1].isLastItem = true
+  }
+}
+
+export default RevealOnScroll
