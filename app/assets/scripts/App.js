e367282b5a8f30472d9562ad816f9916de5ce0c3
Weston <wes.leutz@gmail.com>
2022-08-24 14:02:53 -0500

    Finished Modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,15 @@ import '../styles/styles.css'
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
+import Modal from "./modules/Modal"
 
 if (module.hot) {
   module.hot.accept()
 }
 
 /* Lesson code */
+new Modal()
+
 let mobileMenu = new MobileMenu()
 let stickyHeader = new StickyHeader()
 
